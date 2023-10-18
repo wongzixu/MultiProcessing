db0112b40d345ac52209e6dff704c075d9fd6d28
Wang Zixu <wangzixu530@gmail.com>
2023-10-18 15:15:04 +0800

    [Multiprocess] 8 Process Pool for Matrix

--- matrix_calculation_multiprocess.py
+++ matrix_calculation_multiprocess.py
@@ -1,82 +1,263 @@
+# from matrix_data import MatrixData
+# from prettytable import PrettyTable
+# from multiprocessing import Pool
+# from strassen_algorithm import MatrixMultiplication
+# import numpy as np
+# import time
+
+# class MatrixMultiplicationMultiProcess(object):
+
+#     def __init__(self, pool) -> None:
+#         """ Initialize the computation class, and the target dataset.
+#         Args:
+#             dataset: Unified group matrix dataset used for multiplication.
+#         """
+#         dataset = MatrixData().loaded_matrix_set
+#         pool = pool
+#         pool = Pool(pool)
+#     def __getstate__(self):
+#         self_dict = __dict__.copy()
+#         del self_dict['pool']
+#         return self_dict
+
+#     def __setstate__(self, state):
+#         __dict__.update(state)
+
+#     def matrix_slicing(self, matrix):
+#         """ Matrix slicing into 4 euqal parts.
+#         """
+#         matrix_size = len(matrix)
+#         mid_matrix_size = matrix_size//2
+#         x_11 = matrix[0:mid_matrix_size, 0:mid_matrix_size]
+#         x_12 = matrix[0:mid_matrix_size, mid_matrix_size:matrix_size]
+#         x_21 = matrix[mid_matrix_size:matrix_size, 0:mid_matrix_size]
+#         x_22 = matrix[mid_matrix_size:matrix_size, mid_matrix_size:matrix_size]
+#         return x_11, x_12, x_21, x_22
+    
+#     def multi_processing_matrix_multiply(self, matrix_1, matrix_2):
+#         """ 
+#         Multiply paired matrices with processing pool.
+#         """
+#         # Matrix slicing for each input matrixs, which need to split into 4 parts.
+#         matrix_1 = matrix_1.astype(np.uint64)
+#         matrix_2 = matrix_2.astype(np.uint64)
+
+#         # Linear calculation without using numpy function. threhold
+#         if matrix_1.shape[0] <= 4:
+#             return np.dot(matrix_1, matrix_2)
+
+#         # matrix_1 (x) slicing
+#         x_11, x_12, x_21, x_22 = matrix_slicing(matrix=matrix_1)
+#         # matrix_2 (y) slicing
+#         y_11, y_12, y_21, y_22 = matrix_slicing(matrix=matrix_2)
+
+#         m_1 = multi_processing_matrix_multiply, (x_11 + x_22, y_11 + y_22))
+#         m_2 = multi_processing_matrix_multiply, (x_21 + x_22, y_11))
+#         m_3 = multi_processing_matrix_multiply, (x_11, y_12 - y_22))
+#         m_4 = multi_processing_matrix_multiply, (x_22, y_21 - y_11))
+#         m_5 = multi_processing_matrix_multiply, (x_11 + x_12, y_22))
+#         m_6 = multi_processing_matrix_multiply, (x_21 - x_11, y_11 + y_12))
+#         m_7 = multi_processing_matrix_multiply, (x_12 - x_22, y_21 + y_22))
+
+#         c_11 = m_1 + m_4 - m_5 + m_7
+#         c_12 = m_3 + m_5
+#         c_21 = m_2 + m_4
+#         c_22 = m_1 + m_3 - m_2 + m_6
+
+#         # Result concatation
+#         leading_side = np.vstack((c_11, c_21))
+#         trailing_side = np.vstack((c_12, c_22))
+#         final_c = np.hstack((leading_side, trailing_side))
+
+#         return final_c
+    
+#     def matrices_calculation_multi_process(self):
+#         dataset = dataset
+#         for groupIndex in range(10):
+#             t = np.zeros(8, dtype=np.float64)
+#             table = PrettyTable()
+#             table.title = "Process time of G{} under sequential implementation".format(groupIndex)
+#             table.field_names = ['Pair Index', 'Measured Sequential Time', 'Measured Cumulative Sequential Time']
+#             for pairIndex in range(8):
+#                 t_start = time.time()
+#                 multi_processing_matrix_multiply(dataset[groupIndex][pairIndex][0], dataset[groupIndex][pairIndex][1])
+#                 t_end = time.time()
+#                 t_seconds = t_end-t_start
+#                 t[pairIndex] = t_seconds
+#             for pairIndex in range(8):
+#                 table.add_row([pairIndex, t[pairIndex], sum(t[0:pairIndex+1])])
+#             print(table)
+    
+#     def close_pool(self):
+#         pool.close()
+#         pool.join()
+
+    
+
+# # def main():
+# #     ca2_strassen = MatrixMultiplicationMultiProcess()
+# #     ca2_strassen.matrices_calculation_single_process()
+# #     for pool in range(2, 9):
+# #         ca2_strassen.matrices_calculation_multi_process()
+
+# def test():
+#     """
+#     8 process match the Apple M1 chip 8 cores. For maxmize performance test.
+#     """
+#     ca2_strassen_test = MatrixMultiplicationMultiProcess(pool=8)
+#     ca2_strassen_test.matrices_calculation_multi_process()
+#     ca2_strassen_test.close_pool()
+
+
+
+# if __name__ == "__main__":
+#     test()
+    
+    
 from matrix_data import MatrixData
 from prettytable import PrettyTable
 from multiprocessing import Pool
+import multiprocessing
 from strassen_algorithm import MatrixMultiplication
 import numpy as np
 import time
 
-class MatrixMultiplicationMultiProcess(MatrixMultiplication):
 
-    def __init__(self) -> None:
-        """ Initialize the computation class, and the target dataset.
-        Args:
-            dataset: Unified group matrix dataset used for multiplication.
-        """
-        self.dataset = MatrixData().loaded_matrix_set
+    # def initialize_pool(self):
+    #     # Initialize the pool if it's not already created
+    #     if pool is None:
+    #         if multiprocessing.get_start_method() == 'spawn':
+    #             # Use 'spawn' method to create the pool on Windows
+    #             pool = Pool(pool)
+    #         else:
+    #             pool = Pool(pool, maxtasksperchild=1)
 
-    def strassen_algorithm_multi_multiprocess(self, matrix_1, matrix_2):
-        """ Fundamental strassen's algorithm for matrix multiplication with same 'nxn' size.
-        """
-        # Matrix slicing for each input matrixs, which need to split into 4 parts.
-        matrix_1 = matrix_1.astype(np.uint64)
-        matrix_2 = matrix_2.astype(np.uint64)
+    
+    
+    
 
-        # Linear calculation without using numpy function.
-        if matrix_1.shape[0] == 1:
-            return matrix_1*matrix_2
+    
+    # def close_pool(self):
+    #     pool.close()
+    #     pool.join()
 
-        # matrix_1 (x) slicing
-        x_11, x_12, x_21, x_22 = self._strassen_algorithm_multi__matrix_slicing(matrix=matrix_1)
-        # matrix_2 (y) slicing
-        y_11, y_12, y_21, y_22 = self.__matrix_slicing(matrix=matrix_2)
+# def test(pool):
+#     """
+#     8 process match the Apple M1 chip 8 cores. For maxmize performance test.
+#     """
+#     ca2_strassen_test = MatrixMultiplicationMultiProcess(pool=8)
+#     ca2_strassen_test.matrices_calculation_multi_process(pool=pool)
 
-        m_1 = self.strassen_algorithm_multi(x_11 + x_22, y_11 + y_22)
-        m_2 = self.strassen_algorithm_multi(x_21 + x_22, y_11)
-        m_3 = self.strassen_algorithm_multi(x_11, y_12 - y_22)
-        m_4 = self.strassen_algorithm_multi(x_22, y_21 - y_11)
-        m_5 = self.strassen_algorithm_multi(x_11 + x_12, y_22)
-        m_6 = self.strassen_algorithm_multi(x_21 - x_11, y_11 + y_12)
-        m_7 = self.strassen_algorithm_multi(x_12 - x_22, y_21 + y_22)
 
-        c_11 = m_1 + m_4 - m_5 + m_7
-        c_12 = m_3 + m_5
-        c_21 = m_2 + m_4
-        c_22 = m_1 + m_3 - m_2 + m_6
+def matrix_slicing(matrix):
+    """ Matrix slicing into 4 euqal parts.
+    """
+    matrix_size = len(matrix)
+    mid_matrix_size = matrix_size//2
+    x_11 = matrix[0:mid_matrix_size, 0:mid_matrix_size]
+    x_12 = matrix[0:mid_matrix_size, mid_matrix_size:matrix_size]
+    x_21 = matrix[mid_matrix_size:matrix_size, 0:mid_matrix_size]
+    x_22 = matrix[mid_matrix_size:matrix_size, mid_matrix_size:matrix_size]
+    return x_11, x_12, x_21, x_22
 
-        # Result concatation
-        leading_side = np.vstack((c_11, c_21))
-        trailing_side = np.vstack((c_12, c_22))
-        final_c = np.hstack((leading_side, trailing_side))
+def multi_processing_matrix_multiply(matrix_1, matrix_2):
+    """ 
+    Multiply paired matrices with processing pool.
+    """
+    # Matrix slicing for each input matrixs, which need to split into 4 parts.
+    matrix_1 = matrix_1.astype(np.uint64)
+    matrix_2 = matrix_2.astype(np.uint64)
 
-        return final_c
+    # Linear calculation without using numpy function. threhold
+    if matrix_1.shape[0] == 1:
+        return matrix_1*matrix_2
+    
+    # pool = Pool(pool)
 
-    def matrices_calculation_single_process(self):
-        dataset = self.dataset
+    # matrix_1 (x) slicing
+    x_11, x_12, x_21, x_22 = matrix_slicing(matrix=matrix_1)
+    # matrix_2 (y) slicing
+    y_11, y_12, y_21, y_22 = matrix_slicing(matrix=matrix_2)
 
-        for groupIndex in range(10):
-            t = np.zeros(8, dtype=np.float64)
-            table = PrettyTable()
-            table.title = "Process time of G{} under sequential implementation".format(groupIndex)
-            table.field_names = ['Pair Index', 'Measured Sequential Time', 'Measured Cumulative Sequential Time']
-            for pairIndex in range(4):
-                t_start = time.time()
-                self.strassen_algorithm_multi_multiprocess(dataset[groupIndex][pairIndex][0], dataset[groupIndex][pairIndex][1])
-                t_end = time.time()
-                t_seconds = t_end-t_start
-                t[pairIndex] = t_seconds
-            for pairIndex in range(4):
-                table.add_row([pairIndex, t[pairIndex], sum(t[0:pairIndex+1])])
-            print(table)
+    m_1 = multi_processing_matrix_multiply(x_11 + x_22, y_11 + y_22)
+    m_2 = multi_processing_matrix_multiply(x_21 + x_22, y_11)
+    m_3 = multi_processing_matrix_multiply(x_11, y_12 - y_22)
+    m_4 = multi_processing_matrix_multiply(x_22, y_21 - y_11)
+    m_5 = multi_processing_matrix_multiply(x_11 + x_12, y_22)
+    m_6 = multi_processing_matrix_multiply(x_21 - x_11, y_11 + y_12)
+    m_7 = multi_processing_matrix_multiply(x_12 - x_22, y_21 + y_22)
 
-def main():
-    multi = MatrixMultiplicationMultiProcess()
-    multi.matrices_calculation_single_process()
+    c_11 = m_1 + m_4 - m_5 + m_7
+    c_12 = m_3 + m_5
+    c_21 = m_2 + m_4
+    c_22 = m_1 + m_3 - m_2 + m_6
 
+    # Result concatation
+    leading_side = np.vstack((c_11, c_21))
+    trailing_side = np.vstack((c_12, c_22))
+    final_c = np.hstack((leading_side, trailing_side))
+
+    # pool.close()
+    # pool.join()
+
+    return final_c
+
+def entry_of_matrix_multi(matrix_1, matrix_2, number_of_process):
+    # Matrix slicing for each input matrixs, which need to split into 4 parts.
+    matrix_1 = matrix_1.astype(np.uint64)
+    matrix_2 = matrix_2.astype(np.uint64)
+
+    
+    # matrix_1 (x) slicing
+    x_11, x_12, x_21, x_22 = matrix_slicing(matrix=matrix_1)
+    # matrix_2 (y) slicing
+    y_11, y_12, y_21, y_22 = matrix_slicing(matrix=matrix_2)
+
+    with Pool(number_of_process) as pool:
+        m_1 = pool.apply_async(multi_processing_matrix_multiply, (x_11 + x_22, y_11 + y_22))
+        m_2 = pool.apply_async(multi_processing_matrix_multiply, (x_21 + x_22, y_11))
+        m_3 = pool.apply_async(multi_processing_matrix_multiply, (x_11, y_12 - y_22))
+        m_4 = pool.apply_async(multi_processing_matrix_multiply, (x_22, y_21 - y_11))
+        m_5 = pool.apply_async(multi_processing_matrix_multiply, (x_11 + x_12, y_22))
+        m_6 = pool.apply_async(multi_processing_matrix_multiply, (x_21 - x_11, y_11 + y_12))
+        m_7 = pool.apply_async(multi_processing_matrix_multiply, (x_12 - x_22, y_21 + y_22))
+
+        pool.close()
+        pool.join()
+
+    c_11 = m_1.get() + m_4.get() - m_5.get() + m_7.get()
+    c_12 = m_3.get() + m_5.get()
+    c_21 = m_2.get() + m_4.get()
+    c_22 = m_1.get() + m_3.get() - m_2.get() + m_6.get()
+
+    # Result concatation
+    leading_side = np.vstack((c_11, c_21))
+    trailing_side = np.vstack((c_12, c_22))
+    final_c = np.hstack((leading_side, trailing_side))
+
+    
+
+    return final_c
+
+def matrices_calculation_multi_process(number_of_process, dataset):
+    for groupIndex in range(10):
+        t = np.zeros(8, dtype=np.float64)
+        table = PrettyTable()
+        table.title = "Process time of G{} under sequential implementation".format(groupIndex)
+        table.field_names = ['Pair Index', 'Measured Sequential Time', 'Measured Cumulative Sequential Time']
+        for pairIndex in range(8):
+            t_start = time.time()
+            entry_of_matrix_multi(dataset[groupIndex][pairIndex][0], dataset[groupIndex][pairIndex][1], number_of_process)
+            t_end = time.time()
+            t_seconds = t_end - t_start
+            t[pairIndex] = t_seconds
+        for pairIndex in range(8):
+            table.add_row([pairIndex, t[pairIndex], sum(t[0:pairIndex + 1])])
+        print(table)
 
 if __name__ == "__main__":
-    main()
-    
-    
+    dataset = MatrixData().loaded_matrix_set
+    matrices_calculation_multi_process(8, dataset)
 
 
 
--- strassen_algorithm.py
+++ strassen_algorithm.py
@@ -1,22 +1,112 @@
+from matrix_data import MatrixData
+from prettytable import PrettyTable
+from multiprocessing import Pool
 import numpy as np
+import time
 
-
-class MatrixData:
+class MatrixMultiplication:
 
     def __init__(self) -> None:
-        """
+        """ Initialize the computation class, and the target dataset.
         Args:
-            loaded_matrix_set: The loaded_matrix_set is a three dimentional vector of 2D matrixs.
-            The layout is: loaded_matrix_set[group index][pair index][paired matrix index]
-            group index: [0:9]
-            pair index: [0:7] -> [16:2048]
-            paired matrix index: [0,1], first matrix, second matrix
+            dataset: Unified group matrix dataset used for multiplication.
         """
-        self.loaded_matrix_set = self.__matrix_dataset_initialization('matrix_set.npy')
+        self.dataset = MatrixData().loaded_matrix_set
 
-    def __matrix_dataset_initialization(self, path):
+    def __matrices_add(self, matrix_1, matrix_2):
+        """ Private function for adding two matrices, return the result matrix.
         """
-        Private function for dataset initialization, return target dataset.
+        matrix = np.zeros([len(matrix_1),len(matrix_1)], dtype=np.uint64)
+        for i in range(len(matrix_1)):
+            for j in range(len(matrix_1)):
+                matrix[i,j] = matrix_1[i,j] + matrix_2[i,j]
+        return matrix
+        
+    def __matrices_sub(self, matrix_1, matrix_2):
+        """ Private function for matrix_1 - matrix_2, return the result matrix.
         """
-        return np.load(path, allow_pickle=True)
+        matrix = np.zeros([len(matrix_1),len(matrix_1)], dtype=np.uint64)
+        for i in range(len(matrix_1)):
+            for j in range(len(matrix_1)):
+                matrix[i,j] = matrix_1[i,j] - matrix_2[i,j]
+        return matrix
+    
+    def matrix_slicing(self, matrix):
+        """ Matrix slicing into 4 euqal parts.
+        """
+        matrix_size = len(matrix)
+        mid_matrix_size = matrix_size//2
+        x_11 = matrix[0:mid_matrix_size, 0:mid_matrix_size]
+        x_12 = matrix[0:mid_matrix_size, mid_matrix_size:matrix_size]
+        x_21 = matrix[mid_matrix_size:matrix_size, 0:mid_matrix_size]
+        x_22 = matrix[mid_matrix_size:matrix_size, mid_matrix_size:matrix_size]
+        return x_11, x_12, x_21, x_22
+
+
+    def strassen_algorithm_multi(self, matrix_1, matrix_2):
+        """ Fundamental strassen's algorithm for matrix multiplication with same 'nxn' size.
+        """
+        # Matrix slicing for each input matrixs, which need to split into 4 parts.
+        matrix_1 = matrix_1.astype(np.uint64)
+        matrix_2 = matrix_2.astype(np.uint64)
+
+        # Linear calculation without using numpy function.
+        if matrix_1.shape[0] == 1:
+            return matrix_1*matrix_2
+
+        # matrix_1 (x) slicing
+        x_11, x_12, x_21, x_22 = self.matrix_slicing(matrix=matrix_1)
+        # matrix_2 (y) slicing
+        y_11, y_12, y_21, y_22 = self.matrix_slicing(matrix=matrix_2)
+
+        m_1 = self.strassen_algorithm_multi(x_11 + x_22, y_11 + y_22)
+        m_2 = self.strassen_algorithm_multi(x_21 + x_22, y_11)
+        m_3 = self.strassen_algorithm_multi(x_11, y_12 - y_22)
+        m_4 = self.strassen_algorithm_multi(x_22, y_21 - y_11)
+        m_5 = self.strassen_algorithm_multi(x_11 + x_12, y_22)
+        m_6 = self.strassen_algorithm_multi(x_21 - x_11, y_11 + y_12)
+        m_7 = self.strassen_algorithm_multi(x_12 - x_22, y_21 + y_22)
+
+        c_11 = m_1 + m_4 - m_5 + m_7
+        c_12 = m_3 + m_5
+        c_21 = m_2 + m_4
+        c_22 = m_1 + m_3 - m_2 + m_6
+
+        # Result concatation
+        leading_side = np.vstack((c_11, c_21))
+        trailing_side = np.vstack((c_12, c_22))
+        final_c = np.hstack((leading_side, trailing_side))
+
+        return final_c
+
+    def matrices_calculation_single_process(self):
+        dataset = self.dataset
+
+        for groupIndex in range(10):
+            t = np.zeros(8, dtype=np.float64)
+            table = PrettyTable()
+            table.title = "Process time of G{} under sequential implementation".format(groupIndex)
+            table.field_names = ['Pair Index', 'Measured Sequential Time', 'Measured Cumulative Sequential Time']
+            for pairIndex in range(8):
+                t_start = time.time()
+                self.strassen_algorithm_multi(dataset[groupIndex][pairIndex][0], dataset[groupIndex][pairIndex][1])
+                t_end = time.time()
+                t_seconds = t_end-t_start
+                t[pairIndex] = t_seconds
+            for pairIndex in range(8):
+                table.add_row([pairIndex, t[pairIndex], sum(t[0:pairIndex+1])])
+            print(table)
+
+def main():
+    multi = MatrixMultiplication()
+    multi.matrices_calculation_single_process()
+
+
+if __name__ == "__main__":
+    main()
+    
+    
+
+
+
         
